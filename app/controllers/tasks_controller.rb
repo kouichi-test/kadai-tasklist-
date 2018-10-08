19b9e02ed0a83e43909d8f7e8aa6f7ca541b5795
taro.kirameki <goolden0001@gmail.com>
2018-10-08 15:20:53 +0000

    status column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,7 +25,6 @@ class TasksController < ApplicationController
   end
   
   def edit
-    
   end
   
   def update
@@ -53,7 +52,7 @@ class TasksController < ApplicationController
   end 
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :title)
   end 
 
 end
