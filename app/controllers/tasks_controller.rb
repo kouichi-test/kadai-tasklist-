b206df521a6ac3a3571907ff3a13289803a60a20
taro.kirameki <goolden0001@gmail.com>
2018-10-08 00:25:45 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
@@ -50,6 +52,10 @@ class TasksController < ApplicationController
 
     
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end 
 
   def task_params
     params.require(:task).permit(:content)
